95cc04bed88736de6e929265cb04a56163e57abf
CristobalME96 <cristobal.me96@gmail.com>
2019-04-08 19:51:51 -0400

    update

--- Programacion 1/Tarea 2/3j.py
+++ Programacion 1/Tarea 2/3j.py
@@ -4,13 +4,16 @@ i = 0
 H_roce = alt
 H_sin_roce = alt
 while H_sin_roce > 0.03:
+
     if H_roce > 0.03:
         H_roce = (H_roce * 5/7) - ((H_roce * 5/7) * 0.06)
         if H_roce < 0.03:
             print("Si cae de", alt, "metros con roce, rebota", i, "veces")
+
     if H_sin_roce > 0.03:
         H_sin_roce = (H_sin_roce * 5/7)
-        print(H_sin_roce)
         if H_sin_roce < 0.03:
             print("Si cae de", alt, "metros sin roce, rebota", i, "veces")
+
+    print("con:", H_roce, " sin:", H_sin_roce)
     i += 1
