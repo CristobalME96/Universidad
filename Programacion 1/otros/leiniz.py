6de7a0ef4ead869c79d66bca49b4927c630fd92a
CristobalME96 <cristobal.me96@gmail.com>
2019-04-15 11:46:15 -0400

    progra

---
+++ Programacion 1/otros/leiniz.py
@@ -0,0 +1,5 @@
+num = int(input())
+leiniz
+for i in range (0, n + 1)
+    leiniz += ((-1)**0)/((2*i)+1)
+print(leiniz)
---
+++ Programacion 1/otros/sumas.py
@@ -0,0 +1,10 @@
+negativos
+positivos
+for i in range(0,15)
+num = int(input())
+if num >= 0:
+    positivos += num
+else:
+    negativos += num
+print("Suma negativa: ", negativos)
+print("Suma positiva: ", positivos)
