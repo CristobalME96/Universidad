e897fcddf0401316550fc07730c812a68072470f
Cristobal Oyarce <cristobal.me96@gmail.com>
2019-06-08 18:56:59 -0400

    clean

--- Programacion 1/otros/matrices.py
+++ Programacion 1/otros/matrices.py
@@ -1,9 +1,9 @@
 #A[m][n]
 #B[n][p]
-#A = [[1,2,3],[4,5,6],[7,8,9]]
-#B = [[1,2,3],[-1,-1,-3],[2,2,0]]
-m = len(A[0])
-n = len(A)
+A = [[1,4,7],[2,5,8],[3,6,9]]
+B = [[1,-1,2],[2,-1,2],[3,-3,0]]
+m = len(A)
+n = len(A[0])
 p = len(B)
 C = []
 for i in range(n): # i indica la columna de B
@@ -12,6 +12,6 @@ for i in range(n): # i indica la columna de B
         pos_i = i
         sum = 0
         for k in range(n): #k recorre la fila de A y la columna de B
-            sum += A[k][j] * B[i][k]
+            sum += A[i][k] * B[k][j]
         C[pos_i].append(sum)
 print(C)
