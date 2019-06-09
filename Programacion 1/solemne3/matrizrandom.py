621ee7bfaaacefa695f034444ef754f6955dd2e9
Cristobal Oyarce <cristobal.me96@gmail.com>
2019-06-08 21:02:01 -0400

    ejemplo funciones y matrices

--- Programacion 1/solemne3/matrizrandom.py
+++ Programacion 1/solemne3/matrizrandom.py
@@ -1,11 +1,28 @@
 import random
-m = random.randint(3,10)
-n = random.randint(3,10)
-matriz = []
-for i in range(m):
-    fila = []
-    for j in range(n):
-        fila.append(random.randint(1,100))
-    matriz.append(fila)
-for fila in matriz:
-    print(fila)
+
+def suma_impares(matriz):
+
+    coasadasibaskcnas
+
+    return lista
+
+def promedio_fila(M):
+
+    dskjshdkjsc
+    coasadasibaskcnasasc
+    return lista
+
+# m = random.randint(3,10)
+# n = random.randint(3,10)
+# matriz = []
+# for i in range(m):
+#     fila = []
+#     for j in range(n):
+#         fila.append(random.randint(1,100))
+#     matriz.append(fila)
+# for fila in matriz:
+#     print(fila)
+
+
+print(suma_impares(matriz))
+print(promedio_fila(matriz))
