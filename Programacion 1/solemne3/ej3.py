3cd8cfe7a560930e5c0cc659029020b4b85d4b77
Cristobal Oyarce <cristobal.me96@gmail.com>
2019-06-08 20:37:47 -0400

    algo

--- Programacion 1/solemne3/ej3.py
+++ Programacion 1/solemne3/ej3.py
@@ -1,4 +1,4 @@
-matriz = [[5,0,0],[0,3,0],[0,0,1]]
+matriz = [[5,0,0],[0,3,0],["a",0,"b"]]
 for fila in matriz:
     print(fila)
 for fila in matriz:
@@ -7,3 +7,5 @@ for fila in matriz:
             print(1)
         else:
             print(0)
+print(matriz[-1][0])
+print(matriz[-2][-2])
